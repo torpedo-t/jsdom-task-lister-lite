343b20ac2d63043c5cd5fc7e2b7d9d19face6ed5
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-07-29 14:00:37 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const textField = document.findElementById("new-task-description")
+const tasks = document.findElementById("tasks")
+const form = document.getElementById('create-task-form')
+
+function logSubmit(event) {
+  tasks.textContent = `${textField.textContent}`
+  event.preventDefault();
+}
+
+form.addEventListener("submit", logSubmit);
+
+document.addEventListener("DOMContentLoaded", function() {
+  logSubmit()
 });
